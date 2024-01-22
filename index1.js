8a630293c71c2ddbc7562877466df7ddbf423a97
RosaliaNjoki <njosali0@gmail.com>
2024-01-22 04:10:20 +0300

    mywork

--- index1.js
+++ index1.js
@@ -26,4 +26,38 @@ fetch(hostUrl)
     const beerReviews = document.querySelector(".reviews");
     beerReviews.textContent = data.reviews;
 
+
   }
+// var myBeer;
+  
+const beerUrl = 'http://localhost:3000/beers';
+
+fetch(beerUrl)
+.then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json(); // Assuming the response is JSON
+  })
+  .then((data) => navigation(data))
+  .catch((err) => console.error(`Fetch problem: ${err.message}`));
+
+//fetch ends here 
+function navigation(data){
+    for(let item=0; item < data.length; item++)
+{
+    let navb = document.createElement("ul");
+    navb.className="navb";
+    navb.innerHTML = `
+        <div class = "navigate">
+            <a onClick = "navigate('${data[item].id}')"><h4>${data[item].name}</h4></a>
+        </div>`;
+    document.querySelector('#beer_nav').append(navb);
+
+    document.querySelector('#mybeer').innerHTML = '';
+}
+}
+    
+    
+
+
